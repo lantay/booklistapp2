907137417a77398870259411999f1cc9ed4c6a96
Mason <gmlantay@gmail.com>
2017-10-14 16:06:31 +0800

    messed with booklist component and bookreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  books: booksReducer
 });
 
 export default rootReducer;
+
+        //???????????Stuff don't know //???????????
+        //-where else is that state state state line written
+        //and what does it do?
